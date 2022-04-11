274a0cb6b860d975aac0b7adf410223e0c0d784b
yogox <yogoyogox@gmail.com>
2022-04-11 21:25:34 +0900

    Initial Commit

---
+++ BezierSample/BezierSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierSampleApp.swift
+//  BezierSample
+//
+//  Created by yogox on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct BezierSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
